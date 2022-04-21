2b4a62da008a6009a8cf5d76b2122de225172bc8
Your Name <you@example.com>
2022-04-21 12:14:37 +0300

    bank adding form done

--- js/imanage.js
+++ js/imanage.js
@@ -2,43 +2,61 @@ const openCloseButton = document.querySelector('.open-close-button')
 const bankForm = document.querySelector('.add-bank-form')
 let isBankFormOpened = true
 
-document.querySelector('body').addEventListener('click', function(ev){
+document.querySelector('body').addEventListener("mousedown", function(ev){
     if(ev.target.closest('.open-close-button')){
         if(!isBankFormOpened){
+            ev.stopPropagation()
             isBankFormOpened = true
             let currentValue = 70
             let close = setInterval(()=>{
             bankForm.style.height = `${currentValue}px`
             currentValue+=10
-            if(currentValue==300){clearInterval(close)}
-            }, 10)
+            if(currentValue==450){clearInterval(close)}
+            }, 5)
             openCloseButton.classList.remove('rotate')
-            ev.stopPropagation()
         }
         else{
+            ev.stopPropagation()
             isBankFormOpened = false
-            let currentValue = 300
+            let currentValue = 450
             let close = setInterval(()=>{
             bankForm.style.height = `${currentValue}px`
             currentValue-=10
             if(currentValue==70){clearInterval(close)}
-            }, 10)
+            }, 5)
             openCloseButton.classList.add('rotate')
-            ev.stopPropagation()
         }
     }
     if(!ev.target.closest('.add-bank-form')){
         if(isBankFormOpened){
+            ev.stopPropagation()
             isBankFormOpened = false
-            let currentValue = 300
+            let currentValue = 450
             let close = setInterval(()=>{
             bankForm.style.height = `${currentValue}px`
             currentValue-=10
             if(currentValue==70){clearInterval(close)}
-            }, 10)
+            }, 5)
             openCloseButton.classList.add('rotate')
-            ev.stopPropagation()
         }
     }
     
+})
+
+
+document.getElementById('submit-button').addEventListener('click', function(ev){
+    ev.preventDefault()
+    let bankData = []
+    let isFormValid = true
+    Array.from(document.querySelectorAll('input:invalid')).forEach((el)=>{
+        isFormValid = false
+    })
+    if(isFormValid){
+        Array.from(document.querySelectorAll('input:valid')).forEach((el)=>{
+            if(el.value!='Ok' && el.value!='Reset'){
+                bankData.push(el.value)
+            }
+        })
+        localStorage.setItem(bankData[0], bankData)
+    }
 })
